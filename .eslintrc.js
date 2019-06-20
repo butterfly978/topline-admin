9d02670d2daec6399aaaf5d2cbfd10624004f311
butterfly <butterfly@jj.com>
2019-06-20 20:57:08 +0800

    自定義ESLint代碼校驗規則

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,12 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'space-before-function-paren': ['error', {
+      'anonymous': 'never', // 匿名函數 function () {}
+      'named': 'never', // 命名函數 function too () {}
+      'asyncArrow': 'never' // 箭頭函數 async () => {}
+    }] // 'semi': ['error', 'always']
   },
   parserOptions: {
     parser: 'babel-eslint'
