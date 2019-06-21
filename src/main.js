e956efbf70dd66c3b94d841313b19ba167e74b6a
butterfly <butterfly@jj.com>
2019-06-21 21:34:39 +0800

    用戶登錄-樣式佈局

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 // 先找文件，沒有就找目錄，找到優先加載目錄中的index
 import router from './router'
+// 引入公共樣式文件，最好在 element 文件之後，可以自定義修改 element 內置樣式
+import './styles/index.less'
 
 Vue.use(ElementUI)
 Vue.config.productionTip = false
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,18 +1,75 @@
 <template>
-    <div>
-        <p>Login Component</p>
+  <div class="login-wrap">
+    <div class="form-wrap">
+      <div class="form-head">
+        <img src="./logo_index.png" alt="黑馬頭條號">
+      </div>
+      <el-form class="form-content" ref="form" :model="form">
+        <el-form-item >
+          <el-input placeholder="手機號" v-model="form.mobile"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-col :span="14">
+            <el-input placeholder="驗證碼" v-model="form.code"></el-input>
+          </el-col>
+          <el-col :offset="1" :span="7">
+            <el-button @click="handleSendCode">獲取驗證碼</el-button>
+          </el-col>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="btn-login" type="primary" @click="handleSubmit">登錄</el-button>
+        </el-form-item>
+      </el-form>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'AppLogin',
   data () {
-    return {}
+    return {
+      form: {
+        mobile: '',
+        code: ''
+      }
+    }
+  },
+  methods: {
+    handleSubmit () {
+      console.log('submit!')
+    },
+    handleSendCode () {
+      console.log('code')
+    }
   }
 }
 </script>
 
 <style lang='less' scoped>
-
+.login-wrap {
+  height: 100%;
+  background: url('./login_bg.jpg') center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .form-head {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 15px;
+    img {
+      width: 200px;
+    }
+  }
+  .form-wrap {
+    width: 300px;
+    padding: 25px;
+    background-color: #fff;
+    border-radius: 20px;
+    .btn-login{
+      width: 100%;
+    }
+  }
+}
 </style>
