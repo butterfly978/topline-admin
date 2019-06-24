2402fd8fa1c48ab45614d0da9df5f40373f6f76d
butterfly <butterfly@jj.com>
2019-06-24 19:28:09 +0800

    路由拦截器：访问权限控制&nprogress动效

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import 'nprogress/nprogress.css'
 // 先找文件，沒有就找目錄，找到優先加載目錄中的index
 import router from './router'
 // 引入公共樣式文件，最好在 element 文件之後，可以自定義修改 element 內置樣式
